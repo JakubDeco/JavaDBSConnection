b07a1c1eb12b19138a0c5c5724232d896d081602
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-02-02 11:13:41 +0100

    update City.java: added variables with getters and added constructor;

--- src/sk/kosickaakademia/deco/entity/City.java
+++ src/sk/kosickaakademia/deco/entity/City.java
@@ -3,10 +3,24 @@ package sk.kosickaakademia.deco.entity;
 public class City {
     private String name;
     private int population;
+    private String code3;
+    private String district;
+    private String country;
 
     public City(String name, int population){
         this.name=name;
         this.population=population;
+    }
+
+    public City(String name, int population, String district, String country) {
+        this.name = name;
+        this.population = population;
+        this.district = district;
+        this.country = country;
+    }
+
+    public void setCode3(String code3) {
+        this.code3 = code3;
     }
 
     public String getName() {
@@ -15,5 +29,17 @@ public class City {
 
     public int getPopulation() {
         return population;
+    }
+
+    public String getCode3() {
+        return code3;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCountry() {
+        return country;
     }
 }
