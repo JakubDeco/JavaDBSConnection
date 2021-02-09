084b7418d624b9eb275a18af39a0880d70908500
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-02-09 10:46:51 +0100

    added Server.getMonuments: converting list to json string

--- src/sk/kosickaakademia/deco/Main.java
+++ src/sk/kosickaakademia/deco/Main.java
@@ -3,6 +3,7 @@ package sk.kosickaakademia.deco;
 
 import sk.kosickaakademia.deco.entity.City;
 import sk.kosickaakademia.deco.entity.Country;
+import sk.kosickaakademia.deco.json.Server;
 import sk.kosickaakademia.deco.output.Output;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Database database=new Database("resource/config.properties");
         Output output = new Output();
+        Server server = new Server();
         /*List<City> cities = database.getCities("germany");
         output.printCities(cities);
         Country country1 = database.getCountryInfo("Switzerland");
@@ -20,8 +22,10 @@ public class Main {
         City city1 = new City("Kosice",239171,"Kosicky kraj","Slovakia");
         database.insertCity(city1);
         database.updatePopulation("afghanistan","kabul",4273001);
-        output.printCapitalCities(database.getCapitalCities("north america"));*/
-        //output.printMonuments(database.getMonuments());
+        output.printCapitalCities(database.getCapitalCities("north america"));
+        output.printMonuments(database.getMonuments());
         System.out.println(database.hasCountryCity("france","paris"));
+        System.out.println(database.insertMonument("slovakia", "kosice", "St. Urban's Tower"));*/
+        System.out.println(server.getMonuments());
     }
 }
---
+++ src/sk/kosickaakademia/deco/json/Server.java
@@ -0,0 +1,37 @@
+package sk.kosickaakademia.deco.json;
+
+import org.json.simple.*;
+import sk.kosickaakademia.deco.Database;
+import sk.kosickaakademia.deco.entity.Monument;
+
+import java.util.List;
+
+public class Server {
+    public String getMonuments(){
+        List<Monument> list = new Database("resource/config.properties").getMonuments();
+        String resultJSON = "";
+
+        if (list.isEmpty())
+            return "{}";
+
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+
+        for (Monument temp :
+                list) {
+            JSONObject newObject = new JSONObject();
+            newObject.put("country",temp.getCountry());
+            newObject.put("city",temp.getCity());
+            newObject.put("name",temp.getName());
+            newObject.put("id",temp.getId());
+
+            jsonArray.add(newObject);
+        }
+
+        jsonObject.put("monuments", jsonArray);
+
+        jsonObject.put("count", list.size());
+
+        return jsonObject.toJSONString();
+    }
+}
